2c5edf8fe9ef3e03bc36207fcd38a09ac8f91662
arbaztalk2me <geekyarbaz@gmail.com>
2024-01-04 23:19:13 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typingApp';
+  randomText:string=faker.internet.userName();
+  inputText:string="";
+
+  handleInput(event:any){
+    this.inputText=event.target.value;
+    console.log(this.inputText)
+  }
+
+  getClassName(letter:string,input:string){
+    console.log(input)
+    if(!input){
+      return "greyCol";
+    }
+
+   return letter===input?"succCol":"danCol"
+  }
+
 }
